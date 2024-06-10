bfdf64697ace94e355a8c752114dd806faec92c6
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2024-06-10 15:56:06 -0300

    parcialmente completo

--- Assets/interactionArea.cs
+++ Assets/interactionArea.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class interactionArea : MonoBehaviour
 {
+    int puntaje;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        PickupScript pickup;
+        pickup = other.GetComponent<PickupScript>();
+        puntaje += pickup.PuntosItem;
+        if 
+        Debug.Log("EN CONTACTO");
+    }
+  
     // Update is called once per frame
     void Update()
     {
